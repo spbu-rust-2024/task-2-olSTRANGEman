51bf651fe33191a066e44f23575e41b6afa84ba3
olSTRANGEman <63145815+olSTRANGEman@users.noreply.github.com>
2024-10-09 23:07:55 +0300

    main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,64 @@
-fn main() {
-    for i in 0..1000000{
-        println!("Hello, world!");
+use std::io;
+
+
+
+fn check(line: &str) -> String{
+    let line_b = line.as_bytes();
+    let mut rem = &line[0..1];
+// Как принято итерироваться по строкам nth/bytes/vec?
+    
+    if line.len()%2==1{    
+        for i in 0..line_b.len()-1{
+            for j in 1..=i{
+                if line_b[i+j] != line_b[i-j]{
+                    // не смог объединить два условия для окончания 
+                    if (i+j)-(i-j)-1 > rem.len(){
+                        rem = &line[i-j+1..i+j];
+                    }
+                        break;
+                }
+                if i+j == line_b.len()-1{
+                    if (i+j)-(i-j)-1 > rem.len(){
+                        rem = &line[i-j..i+j+1];
+                    }
+                        break;
+                }
+            }
+        }
+
+    }else{
+
+        for i in 0..line_b.len()-1{
+            for j in 1..=i{
+                if line_b[i+j-1] != line_b[i-j]{
+                    if (i+j-1)-(i-j)-1 > rem.len(){
+                        rem = &line[i-j+1..i+j-1];
+                    }
+                        break;
+                }
+                if i+j-1 == line_b.len()-1{
+                    if (i+j-1)-(i-j)-1 > rem.len(){
+                        rem = &line[i-j..i+j];
+                    }
+                        break;
+                }
+            }
+        }
     }
+    
+    
+    rem.to_string()
+    }
+
+//разница &str\&String\str\String?
+
+
+
+fn main(){
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Input is not a string");
+    let input = input.trim();
+    println!("{}",check(&input));
 }
